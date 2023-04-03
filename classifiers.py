291d7277f1be068f70afadc08a405bf5bfa12634
AdhamWaelSalem <94129835+AdhamWaelSalem@users.noreply.github.com>
2023-04-03 14:19:54 +0200

    Add files via upload

---
+++ classifiers.py
@@ -0,0 +1,108 @@
+import numpy as np
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from matplotlib import pyplot
+
+from dataFunctions import readCsv, split, crossValidate
+
+
+def decisionTree(X_train, X_test, y_train, y_test):
+    clf = DecisionTreeClassifier().fit(X_train, y_train)
+    y_predict = clf.predict(X_test)
+    reportClassifier(y_test, y_predict)
+
+
+def naiveBayes(X_train, X_test, y_train, y_test):
+    clf = GaussianNB().fit(X_train, y_train)
+    y_predict = clf.predict(X_test)
+    reportClassifier(y_test, y_predict)
+
+
+def knn(X_train, X_test, y_train, y_test):
+    hyperParam = [1, 1, 0]
+    # acc_data = []
+    for k in range(7,8):  # Tested in range(1, 200)  and 7 was highest accuracy
+        clf = KNeighborsClassifier(n_neighbors=k)
+        acc_list_temp = crossValidate(X_train, y_train, clf)
+        # acc_data.append(acc_list_temp[1])
+        if acc_list_temp[1] > hyperParam[2]:
+            hyperParam = [k, acc_list_temp[0], acc_list_temp[1]]
+    # pyplot.plot(range(5, 205, 5), acc_data)
+    # pyplot.title('KNN with K Tuned')
+    # pyplot.xlabel("K")
+    # pyplot.ylabel("Accuracy")
+    # pyplot.show()
+    # print(f'Best k = {hyperParam[0]} with best accuracy = {hyperParam[2]}')
+    clf = KNeighborsClassifier(n_neighbors=hyperParam[0])
+    clf.fit(X_train, y_train)
+    y_predict = clf.predict(X_test)
+    reportClassifier(y_test, y_predict)
+
+
+def adaBoost(X_train, X_test, y_train, y_test):
+    hyperParam = [1, 1, 0]
+    # acc_data = []
+    for n in range(165, 166):  # Tested in range(1, 200)  and 165 was highest accuracy
+        clf = AdaBoostClassifier(n_estimators=n)
+        acc_list_temp = crossValidate(X_train, y_train, clf)
+        # acc_data.append(acc_list_temp[1])
+        if acc_list_temp[1] > hyperParam[2]:
+            hyperParam = [n, acc_list_temp[0], acc_list_temp[1]]
+    # pyplot.plot(range(5, 205, 5), acc_data)
+    # pyplot.title('Adaboost with n-estimators')
+    # pyplot.xlabel("n estimators")
+    # pyplot.ylabel("Accuracy")
+    # pyplot.show()
+    # print(f'Best n = {hyperParam[0]} with best accuracy = {hyperParam[2]}')
+    clf = AdaBoostClassifier(n_estimators=hyperParam[0])
+    clf.fit(X_train, y_train)
+    y_predict = clf.predict(X_test)
+    reportClassifier(y_test, y_predict)
+
+
+def randomForest(X_train, X_test, y_train, y_test):
+    hyperParam = [1, 1, 0]
+    # acc_data = []
+    for n in range(75, 76):  # Tested in range(1, 200)  and 75 was highest accuracy
+        clf = RandomForestClassifier(n_estimators=n, max_depth=3)
+        acc_list_temp = crossValidate(X_train, y_train, clf)
+        # acc_data.append(acc_list_temp[1])
+        if acc_list_temp[1] > hyperParam[2]:
+            hyperParam = [n, acc_list_temp[0], acc_list_temp[1]]
+    # pyplot.plot(range(5, 205, 5), acc_data)
+    # pyplot.title('Random Forest with n-estimators')
+    # pyplot.xlabel("n estimators")
+    # pyplot.ylabel("Accuracy")
+    # pyplot.show()
+    # print(f'Best n = {hyperParam[0]} with best accuracy = {hyperParam[2]}')
+    clf = RandomForestClassifier(max_depth=hyperParam[0])
+    clf.fit(X_train, y_train)
+    y_predict = clf.predict(X_test)
+    reportClassifier(y_test, y_predict)
+
+
+def reportClassifier(y_test, y_pred):
+    TG, FG = confusion_matrix(y_test, y_pred)[0]
+    FH, TH = confusion_matrix(y_test, y_pred)[1]
+    print("Confusion Matrix:\n")
+    print(f'TG:{TG}\tFG:{FG}\nFH:{FH}\tTH:{TH}\n')
+    print(f"Accuracy: {accuracy_score(y_test, y_pred) * 100}\n")
+    print('Report:')
+    print(f"{classification_report(y_test, y_pred)}\n", )
+
+
+def classify():
+    dataFrame = readCsv()
+    X_train, X_test, y_train, y_test = split(dataFrame)
+    print(f'AFTER SPLITTING:\nBALANCE CHECK FOR TRAIN:\n{y_train.value_counts()}\n\n')
+    print(f'BALANCE CHECK FOR TEST:\n{y_test.value_counts()}\n\n')
+    y_train = np.ravel(y_train, order='C')  # to avoid warnings
+    classifiers = {'DECISION TREE': decisionTree, 'NAIVE BAYES': naiveBayes, 'KNN': knn, 'ADABOOST': adaBoost,
+                   'RANDOM FOREST': randomForest}
+    for classifierName, classifier in classifiers.items():
+        print(f'*************************** CLASSIFICATION USING {classifierName} ***************************\n\n')
+        classifier(X_train, X_test, y_train, y_test)
---
+++ dataFunctions.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+from imblearn.datasets import make_imbalance
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+
+
+def readCsv():
+    return pd.read_csv('magic04.data')
+
+
+def split(dataFrame):
+    x, y = dataFrame.iloc[:, :-1], dataFrame.iloc[:, [-1]]  # split features and label
+    x, y = make_imbalance(x, y, sampling_strategy={'g': 6688, 'h': 6688}, random_state=14)
+    return train_test_split(x, y, test_size=0.3, train_size=0.7, random_state=17, stratify=y)
+
+
+def crossValidate(X_train, y_train, classifier):
+    acc_list = [0, 0]
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    print('IN CROSS VALIDATION:\n\n')
+    for fold in range(19, 20):  # Tested in range(2, 100)  and 19 was highest accuracy
+        kf = KFold(n_splits=fold)
+        kf.get_n_splits(X_train)
+        acc = 0
+        counter = 0
+        for train_index, test_index in kf.split(X_train):
+            KFX_train, KFX_test = X_train[train_index], X_train[test_index]
+            KFy_train, KFy_test = y_train[train_index], y_train[test_index]
+            print(f'BALANCE CHECK FOR TRAIN:\n')
+            countLabels(KFy_train)
+            print(f'BALANCE CHECK FOR TEST:\n')
+            countLabels(KFy_test)
+            classifier.fit(KFX_train, KFy_train)
+            KFy_predict = classifier.predict(KFX_test)
+            acc += accuracy_score(KFy_test, KFy_predict) * 100
+            counter += 1
+        acc = acc / counter
+        if acc > acc_list[1]:
+            acc_list = [fold, acc]
+    return acc_list
+
+
+def countLabels(y):
+    labelCount = {'g': 0, 'h': 0}
+    for label in y:
+        labelCount[label] += 1
+    print(f"g:{labelCount['g']}")
+    print(f"h:{labelCount['h']}\n\n")
